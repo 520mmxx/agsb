6d387f887e48fa6030652019dba2319d612ffa68
520mmxx <116633404+520mmxx@users.noreply.github.com>
2025-06-06 03:46:03 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -32,10 +32,10 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+UUID = "88c14aa7-7cf2-40fc-9e4a-0f51"                     # UUID，留空则自动生成
+PORT = 25604                   # Vmess端口，留空或0则自动生成
+DOMAIN = "appidx.ioooi.filegear-sg.me"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiZDllMGM0MGNjZDY1MzVmZWM5NGZjNDY3MjA1MzRhZDUiLCJ0IjoiYjhkZDI1ODAtNWY3MS00MzQzLWExZTEtZjZlYWQ2ZjAwYjZhIiwicyI6Ik4yVXdNekkxTlRBdE9EaGlZaTAwWVRNM0xXSmlOVEV0TkdZeE5EUXpNR0U0T1dGaCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
